69b733d6ee19bd8c5f31e9442db40e191d0f47e4
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-04-22 19:37:38 +0300

    Create weblayerTest and ApplicationTests

--- src/test/java/hh/linneamyl/coronahelper/CoronahelperApplicationTests.java
+++ src/test/java/hh/linneamyl/coronahelper/CoronahelperApplicationTests.java
@@ -1,13 +1,29 @@
 package hh.linneamyl.coronahelper;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import hh.linneamyl.coronahelper.web.CoronahelperController;
+
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CoronahelperApplicationTests {
-
+	
+	// Testaa ettei ohjelman controller ole tyhjä
+	
+	@Autowired
+	private CoronahelperController controller;
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
+		   
 	}
 
 }
